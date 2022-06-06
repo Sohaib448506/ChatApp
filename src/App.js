7c9852e87196b45414cdd8d9a04e19b84b9ef4ea
Sohaib <sohaibbutt448506@gmail.com>
2022-06-06 22:14:01 +0500

    Chat App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useMemo, useState } from "react";
+import "./App.css";
+
+import { getDatabase, ref, push, set, onChildAdded } from "firebase/database";
 
 function App() {
+  const db = getDatabase();
+  const chatList = ref(db, "List");
+  const [name, setName] = useState("");
+  const [chats, setChats] = useState([]);
+  const [msg, setMsg] = useState("");
+
+  const sendChat = async () => {
+    const chatRef = await push(chatList);
+    await set(chatRef, {
+      name: name,
+      message: msg,
+    });
+    setMsg("");
+    updateHeight();
+  };
+  const updateHeight = () => {
+    const el = document.getElementById("chat");
+    if (el) {
+      el.scrollTop = el.scrollHeight + el.clientHeight;
+    }
+  };
+
+  useMemo(() => {
+    onChildAdded(chatList, (data) => {
+      setChats((chats) => [...chats, data.val()]);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      {name ? null : (
+        <div className="nameContainer">
+          <input
+            id="myInput"
+            type="text"
+            placeholder="Enter Your name"
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                setName(e.target.value);
+              }
+            }}
+            onBlur={(e) => setName(e.target.value)}
+            autoFocus
+          />
+          <button>Submit</button>
+        </div>
+      )}
+
+      {name ? (
+        <div>
+          <div className="headingDiv">
+            <h1> {name}</h1>
+          </div>
+
+          <div id="chat" className="chat-container">
+            {chats.map((c, i) => (
+              <div
+                key={i}
+                className={`container ${c.name === name ? "me" : ""}`}
+              >
+                <p className="chatbox">
+                  <strong>{c.name}: </strong>
+                  <span>{c.message}</span>
+                </p>
+              </div>
+            ))}
+          </div>
+          <div className="btm">
+            <input
+              type="text"
+              placeholder="enter your message"
+              onInput={(e) => setMsg(e.target.value)}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  sendChat();
+                }
+              }}
+              value={msg}
+              autoFocus
+            />
+            <button onClick={sendChat}>send</button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCS5-S2IzsaD8q3o3ieCN5ZD10TshKqbl8",
+  authDomain: "chat-2b75a.firebaseapp.com",
+  databaseURL: "https://chat-2b75a-default-rtdb.firebaseio.com",
+  projectId: "chat-2b75a",
+  storageBucket: "chat-2b75a.appspot.com",
+  messagingSenderId: "1080211250878",
+  appId: "1:1080211250878:web:ed903e854dd19f916308ef",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
